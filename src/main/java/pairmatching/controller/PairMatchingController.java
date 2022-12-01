45ffe112ac6d1b339bc9fa43f7d330f159fd763a
parkjungyoon <tbr06057@naver.com>
2022-12-01 12:47:15 +0900

    refactor: 에러 발생 시 메세지 출력하고 다시 입력받는 기능 추가

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,15 +1,21 @@
 package pairmatching.controller;
 
 import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
 
 import static pairmatching.validator.CommandValidator.validateInvalidMenuCommand;
 
 public class PairMatchingController {
 
     private String initMenuCommand() {
-        String command = InputView.readMenuCommand();
-
-        validateInvalidMenuCommand(command);
+        String command;
+        try {
+            command = InputView.readMenuCommand();
+            validateInvalidMenuCommand(command);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return initMenuCommand();
+        }
         return command;
     }
 }
