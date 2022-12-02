53ef3afda57fc59418bfcf1d1ec4d163c8e3d811
parkjungyoon <tbr06057@naver.com>
2022-12-02 14:00:37 +0900

    feat: Pair를 생성하는 메서드 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -2,6 +2,7 @@ package pairmatching.controller;
 
 import pairmatching.domain.Course;
 import pairmatching.domain.Crew;
+import pairmatching.domain.Pair;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
 
@@ -12,12 +13,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Math.min;
 import static pairmatching.validator.CommandValidator.validateInvalidMenuCommand;
 
 public class PairMatchingController {
     public void startPairMatching() {
         List<Crew> frontEndCrew = initCrew(Course.FRONTEND, "./src/main/resources/frontend-crew.md");
         List<Crew> backEndCrew = initCrew(Course.BACKEND, "./src/main/resources/backend-crew.md");
+        createPair(backEndCrew);
+    }
+
+    private void createPair(List<Crew> shuffledCrew) {
+        List<Pair> pairMatching = new ArrayList<>();
+        int limit = 2;
+        for(int id = 0; id < shuffledCrew.size(); id += limit){
+            if (shuffledCrew.size() % 2 == 1 && id == shuffledCrew.size() - 3) {
+                List<Crew> pair = new ArrayList<>(shuffledCrew.subList(id, shuffledCrew.size()));
+                pairMatching.add(new Pair(pair));
+                break;
+            }
+            List<Crew> pair = new ArrayList<>(shuffledCrew.subList(id, min(id + limit, shuffledCrew.size())));
+            pairMatching.add(new Pair(pair));
+        }
+        for (Pair pair : pairMatching) {
+            System.out.println(pair.toString());
+        }
     }
 
     private List<Crew> initCrew(Course course, String pathName) {
