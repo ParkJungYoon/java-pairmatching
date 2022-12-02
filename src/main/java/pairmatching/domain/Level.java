6f4b8ea64aef6357eb43da5690e4c0afe5de37d2
parkjungyoon <tbr06057@naver.com>
2022-12-02 13:07:24 +0900

    fix: 미션이 없는 레벨에 미션은 으로 리스트 추가

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -6,9 +6,9 @@ import java.util.List;
 public enum Level {
     LEVEL1("레벨1", Arrays.asList("자동차경주", "로또", "숫자야구게임")),
     LEVEL2("레벨2", Arrays.asList("장바구니","결제","지하철노선도")),
-    LEVEL3("레벨3", List.of()),
+    LEVEL3("레벨3", Arrays.asList("")),
     LEVEL4("레벨4", Arrays.asList("성능개선","배포")),
-    LEVEL5("레벨5", List.of());
+    LEVEL5("레벨5", Arrays.asList(""));
 
     private final String name;
     private final List<String> missionName;
