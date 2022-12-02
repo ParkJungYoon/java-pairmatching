0b0829326b0e42a8613515320fd203d8443921ff
parkjungyoon <tbr06057@naver.com>
2022-12-02 23:11:22 +0900

    feat: 초기화 기능 구현 완료

--- src/main/java/pairmatching/controller/MenuController.java
+++ src/main/java/pairmatching/controller/MenuController.java
@@ -7,15 +7,24 @@ import pairmatching.view.OutputView;
 import static pairmatching.validator.CommandValidator.validateInvalidMenuCommand;
 
 public class MenuController {
+    private static final String PAIR_MATCHING = "1";
+    private static final String PAIR_LOOKUP = "2";
+    private static final String INITIALIZATION = "3";
+    private static final String QUIT = "Q";
     private static final PairMatchingController pairMatchingController = new PairMatchingController();
 
     public void selectMenu() {
-        PairMatchingResult pairMatchingResult = new PairMatchingResult();
-
         String menu = initMenuCommand();
-        if (menu.equals("1")) {
-//            startPairMatching(pairMatchingResult, );
+        if (menu.equals(PAIR_MATCHING)) {
+            pairMatchingController.startPairMatching();
+        } else if (menu.equals(PAIR_LOOKUP)) {
+//            pairMatchingController.startPairLookUp;
+        } else if (menu.equals(INITIALIZATION)) {
+            pairMatchingController.startInitialization();
+        } else if (menu.equals(QUIT)) {
+            return;
         }
+        selectMenu();
     }
 
     private String initMenuCommand() {
