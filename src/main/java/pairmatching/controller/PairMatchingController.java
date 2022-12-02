12dafd080bf97de9a14ca91d6e3c24fb68a5ed7c
parkjungyoon <tbr06057@naver.com>
2022-12-02 13:08:33 +0900

    feat: 크루들의 이름 목록 파일을 로드해서 List 생성

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,11 +1,41 @@
 package pairmatching.controller;
 
+import pairmatching.domain.Course;
+import pairmatching.domain.Crew;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static pairmatching.validator.CommandValidator.validateInvalidMenuCommand;
 
 public class PairMatchingController {
+    public void startPairMatching() {
+        List<Crew> frontEndCrew = initCrew(Course.FRONTEND, "./src/main/resources/frontend-crew.md");
+        List<Crew> backEndCrew = initCrew(Course.BACKEND, "./src/main/resources/backend-crew.md");
+    }
+
+    private List<Crew> initCrew(Course course, String pathName) {
+        List<Crew> crew = new ArrayList<>();
+
+        File file = new File(pathName);
+        try (BufferedReader br = new BufferedReader(new FileReader(file)))
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+                crew.add(new Crew(course, line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return crew;
+    }
+
 
     private String initMenuCommand() {
         String command;
