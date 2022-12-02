27b6e8cf0bacd70210345c0b1554b5d6363d470d
parkjungyoon <tbr06057@naver.com>
2022-12-02 23:09:40 +0900

    refactor: 결과 저장하는 객체를 정적 메서드로 변경

--- src/main/java/pairmatching/domain/PairMatchingResult.java
+++ src/main/java/pairmatching/domain/PairMatchingResult.java
@@ -2,27 +2,28 @@ package pairmatching.domain;
 
 import pairmatching.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PairMatchingResult {
-    private final Map<String, List<Pair>> frontEndResult = new HashMap<>();
-    private final Map<String, List<Pair>> backEndResult = new HashMap<>();
+    private static final Map<String, List<Pair>> frontEndResult = new HashMap<>();
+    private static final Map<String, List<Pair>> backEndResult = new HashMap<>();
 
-    public boolean hasMatchingResult(Course course, String missionName) {
+    public static boolean hasMatchingResult(Course course, String missionName) {
         if (course == Course.FRONTEND && frontEndResult.containsKey(missionName)) return true;
         if (course == Course.BACKEND && backEndResult.containsKey(missionName)) return true;
         return false;
     }
 
-    public void savePairMatchingResult(Course course, String missionName, List<Pair> pairMatchingResult) {
+    public static void savePairMatchingResult(Course course, String missionName, List<Pair> pairMatchingResult) {
         if (course == Course.FRONTEND) frontEndResult.put(missionName, pairMatchingResult);
         if (course == Course.BACKEND) backEndResult.put(missionName, pairMatchingResult);
     }
 
-    public void printPairMatching(Course course, String missionName) {
-        List<Pair> pairResult = null;
+    public static void printPairMatching(Course course, String missionName) {
+        List<Pair> pairResult = new ArrayList<>();
         if (course == Course.FRONTEND) pairResult = frontEndResult.get(missionName);
         if (course == Course.BACKEND) pairResult = backEndResult.get(missionName);
 
